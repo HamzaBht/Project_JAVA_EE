20dcfd38dcec5fb22ad482a79f4c4aee7df07bfb
fakhri Moad <moad_fakhri_1999@outlook.com>
2021-03-06 22:35:46 +0100

    close db connections after CRUD operations

--- flight_api/src/main/java/com/flight/dao/implementations/AirportDAOImplementation.java
+++ flight_api/src/main/java/com/flight/dao/implementations/AirportDAOImplementation.java
@@ -5,6 +5,7 @@ import com.flight.dao.IDAO;
 import com.flight.api.beans.Airport;
 import com.flight.api.beans.City;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -33,66 +34,109 @@ public class AirportDAOImplementation implements IDAO<Airport> {
     }
     @Override
     public int Create(Airport airport) throws DAOException {
+        Connection connection = null;
+        PreparedStatement prep;
         try {
             String sql="insert into aeroport(nom,ville_idville) values(?,?) ";
-            PreparedStatement prep= instance.getConnection().prepareStatement(sql);
+            connection = instance.getConnection();
+            prep= connection.prepareStatement(sql);
             SetStringPrep(airport,prep);
-            return prep.executeUpdate();
+            int status = prep.executeUpdate();
+            connection.close();
+            return status;
         } catch (SQLException e) {
             throw new DAOException(e);
+        }
+        finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException throwables) {
+                    throw new DAOException(throwables);
+                }
+            }
         }
     }
 
     @Override
     public Airport Find(int id) throws DAOException {
+        Connection connection = null;
         try{
             String sql="select * from aeroport where idaeroport=?  limit 1";
-            PreparedStatement prep=instance.getConnection().prepareStatement(sql);
+            connection = instance.getConnection();
+            PreparedStatement prep= connection.prepareStatement(sql);
             prep.setString(1,String.valueOf(id));
             ResultSet res=prep.executeQuery();
             Airport airport=null;
             if(res.next()){
                 airport = map(res);
             }
+            connection.close();
             return airport;
         } catch (SQLException e) {
             throw new DAOException(e);
+        }
+        finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException throwables) {
+                    throw new DAOException(throwables);
+                }
+            }
         }
     }
 
     @Override
     public boolean Update(Airport airport) throws DAOException {
+        Connection connection = null;
         try {
+            connection = instance.getConnection();
             String sql="update aeroport set nom=? and ville_idVille=? where idaeroport=?";
-            PreparedStatement prep=instance.getConnection().prepareStatement(sql);
+            PreparedStatement prep = connection.prepareStatement(sql);
             SetStringPrep(airport,prep);
             prep.setString(3,String.valueOf(airport.getID()));
-            try {
-                prep.executeUpdate();
-            }catch (Exception e){
-                return false;
-            }
+            prep.executeUpdate();
+            connection.close();
             return true;
         }
         catch (SQLException e) {
             throw new DAOException(e);
+        }
+        finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException throwables) {
+                    throw new DAOException(throwables);
+                }
+            }
         }
     }
 
     @Override
     public boolean Remove(int id) throws DAOException {
+        PreparedStatement prep;
+        Connection connection = null;
         try{
-            PreparedStatement prep=instance.getConnection().prepareStatement("delete from aeroport where idaeroport=?");
+            connection = instance.getConnection();
+            prep=connection.prepareStatement("delete from aeroport where idaeroport=?");
             prep.setString(1, String.valueOf(id));
-            try{
-                prep.executeUpdate();
-            }catch (Exception e){
-                return  false;
-            }
+            prep.executeUpdate();
+            connection.close();
             return true;
         }
         catch (SQLException e) {
             throw new DAOException(e);
+        }
+        finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException throwables) {
+                    throw new DAOException(throwables);
+                }
+            }
         }
     }
 
