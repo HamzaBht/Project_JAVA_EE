f79a3494ee686ab64434f6e5c7efdc05a4039a6a
fakhri Moad <moad_fakhri_1999@outlook.com>
2021-06-10 11:04:34 +0100

    add more unit tests

--- Fligght/src/test/java/com/fligght/api/implementations/KamehamehaAPITest.java
+++ Fligght/src/test/java/com/fligght/api/implementations/KamehamehaAPITest.java
@@ -2,32 +2,22 @@ package com.fligght.api.implementations;
 
 import com.fligght.beans.CabineClass;
 import com.fligght.beans.City;
+import com.fligght.beans.Flight;
 import com.fligght.beans.SearchQuery;
+import com.fligght.controllers.SearchRequestController;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class KamehamehaAPITest {
 
-    SearchQuery createTestQuery()
-    {
-        SearchQuery searchQuery = new SearchQuery();
-        searchQuery.setChildrenCount(0);
-        searchQuery.setCabineClass(CabineClass.OTHER);
-        SearchQuery.SearchDepartureArrivalInfo arrivalInfo = new SearchQuery.SearchDepartureArrivalInfo();
-        searchQuery.setArrival(arrivalInfo);
-        City city = new City();
-        city.setName("Paris");
-        arrivalInfo.setCity(city);
-        return searchQuery;
-    }
-
     @Test
     void searchQueryToJson() {
         //create a search query test data
-        SearchQuery query = createTestQuery();
+        SearchQuery query = SearchRequestController.GetSearchQueryFromRequest("","","","","");
         String result = KamehamehaAPI.getInstance().SearchQueryToJson(query);
         assertNotNull(result);
         System.out.println(result);
@@ -35,10 +25,47 @@ class KamehamehaAPITest {
 
     @Test
     void getFlights() {
-        SearchQuery query = createTestQuery();
-        ArrayList result = KamehamehaAPI.getInstance().GetFlights(query);
+        SearchQuery query = SearchRequestController.GetSearchQueryFromRequest("","","","","");
+        ArrayList<Flight> result = KamehamehaAPI.getInstance().GetFlights(query);
         assertNotNull(result);
         System.out.println("result size : " + result.size());
+
     }
 
+    @Test
+    void getFlightWithClass()
+    {
+        SearchQuery query = SearchRequestController.GetSearchQueryFromRequest("","","","","Economie");
+        ArrayList<Flight> result = KamehamehaAPI.getInstance().GetFlights(query);
+        assertNotNull(result);
+        System.out.println("result size : " + result.size());
+        for (Flight flight : result){
+            assertEquals(flight.getCabine().getType(),CabineClass.Economy);
+        }
+    }
+
+    @Test
+    void getFlightWithArrival()
+    {
+        SearchQuery query = SearchRequestController.GetSearchQueryFromRequest("","Paris","","","");
+        ArrayList<Flight> result = KamehamehaAPI.getInstance().GetFlights(query);
+        assertNotNull(result);
+        System.out.println("result size : " + result.size());
+        for (Flight flight : result){
+            System.out.println(flight.getArrivalInfo().getAirport().getName().toLowerCase());
+            assertTrue(flight.getArrivalInfo().getAirport().getName().toLowerCase().contains("paris"));
+        }
+    }
+    @Test
+    void getFlightWithDepart()
+    {
+        SearchQuery query = SearchRequestController.GetSearchQueryFromRequest("Rabat","","","","");
+        ArrayList<Flight> result = KamehamehaAPI.getInstance().GetFlights(query);
+        assertNotNull(result);
+        System.out.println("result size : " + result.size());
+        for (Flight flight : result){
+            System.out.println(flight.getDepartureInfo().getAirport().getName().toLowerCase());
+            assertTrue(flight.getDepartureInfo().getAirport().getName().toLowerCase().contains("rabat"));
+        }
+    }
 }
